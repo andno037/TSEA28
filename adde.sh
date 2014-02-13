4cac05da9f8e1c954f4018b201cab983e45608e8
andno037 <andno037@student.liu.se>
2014-02-13 11:04:28 +0100

    adde.sh update

--- adde.sh
+++ adde.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-/site/edu/da/sw/c6/bin/m68k-as -M -m68000 -al -o tmp.o ./$1 ./$2 >./$1.lst
+/site/edu/da/sw/c6/bin/m68k-as -M -m68000 -al -o tmp.o ./$1 >./$1.lst
 /site/edu/da/sw/c6/bin/m68k-ld --entry=1000 --oformat=coff-m68k -Ttext=1000 ./tmp.o
 
+mv a.out ~
